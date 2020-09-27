26961b6955199a5b85434bad7acbf76dbf9bfc26
manticorevault <artur_seilah@hotmail.com>
2020-09-27 13:32:22 +0100

    Added the PaymentSchema

--- backend/db.js
+++ backend/db.js
@@ -5,3 +5,15 @@ mongoose.connect(
     //INSERT the connection string here
     { useNewUrlParses: true, useUnifiedTopology: true }
 );
+
+const paymentSchema = new mongoose.Schema({
+    id: String,
+    itemId: String,
+    paid: Boolean
+});
+
+const Payment = new mongoose.model("Payment", paymentSchema);
+
+module.exports = {
+    Payment
+}
